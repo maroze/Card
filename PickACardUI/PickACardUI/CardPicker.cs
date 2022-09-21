4f97e37e757ccde451af4171be1c5ecfd03692d6
maroze <91451262+maroze@users.noreply.github.com>
2022-09-21 23:13:20 +0300

    app random cards

---
+++ PickACardUI/PickACardUI/CardPicker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickACardUI
+{
+    public class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int NumberOfCards)
+        {
+            string[] pickedCards = new string[NumberOfCards];
+
+            for (int i = 0; i < NumberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        public static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+
+            switch (value)
+            {
+                case (1):
+                    return "Пики";
+                    
+                case (2):
+                    return "Черви";
+                    
+                case (3):
+                    return "Крести";
+                    
+                default:
+                    return "Бубны";
+            }
+        }
+       public static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+
+            if (value == 1) return "Туз";
+            if (value == 11) return "Валет";
+            if (value == 12) return "Дама";
+            if (value == 13) return "Король";
+
+            return value.ToString( );
+        }
+    }
+}
---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество карт: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Введенное значение не является целым числом");
+            }
+        }
+    }
+}
